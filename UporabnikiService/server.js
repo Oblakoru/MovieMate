0638dd97b2c8f0dff8695058377c99f4edb36a3f
Benjamin Jeran <108634496+Oblakoru@users.noreply.github.com>
2025-03-23 11:37:05 +0100

    Dodan dockerfile za user-mikrostoritev ter skupen compose file. - Implementiran tudi logging

--- UporabnikiService/server.js
+++ UporabnikiService/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const usersRouter = require("./routes/users");
+const morgan = require("morgan");
 const UserRepository = require("./src/infrastructure/UserRepository");
 
 const app = express();
 app.use(express.json());
 
+
+app.use(morgan('dev')); 
+
 const userRepository = new UserRepository();
 
-app.use("/users", usersRouter(userRepository)); r
+app.use("/users", usersRouter(userRepository)); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Server teče na http://localhost:${PORT}`));
